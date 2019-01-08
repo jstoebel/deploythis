c5a1d3def07e42d9604692cd8ee40dcb1a9f4857
Jacob Stoebel <jstoebel@epublishing.com>
2019-01-08 09:31:09 -0500

    whatever

--- lib/deploythis.rb
+++ lib/deploythis.rb
@@ -46,4 +46,26 @@ module Deploythis
       system("say '#{options['project_name']} deploy #{results ? 'complete' : 'failed'}'")
     end
   end
+
+#   yeah, I have a shell function to get that info by dumping the staging db config as json
+# it needs to be run on the server
+# it requires a program called `jq` to filter JSON
+# but you could modify it to just get the properties you want by editing the ruby code it runs instead
+# ```function json-db-config() {
+#   exec 3>&2
+#   exec 2> /dev/null
+#   local environment="${1:-staging}"
+#   RAILS_ENV="$environment" /opt/ruby/bin/bundle exec rails runner 'puts Jade::Engine.configure_database.to_json' | jq ".${environment}"
+#   exec 2>&3
+# }```
+# instead of converting that whole db config object to json, you could just grab the key for the environment you're looking for
+# or install jq into your home folder on the server
+# it's a small standalone CLI program
+# This gives you the DB config object for an app, and the stuff in quotes at the end is Ruby, so you can manipulate it however you want
+# `RAILS_ENV="$environment" /opt/ruby/bin/bundle exec rails runner 'puts Jade::Engine.configure_database'` (edited)
+# This isn't good for production databases though, because the host property of that object will point to the master, and you always want to dump from the slave
+
+# You can derive the slave hostname with sed: `db_host="$(echo $db_host | sed -re 's/^([^.]+)/\1-slave/')"`
+
+# what about `ssh jstoebel@rails-stage-app-2 'cd /web/project/provisioneronline_staging/current && RAILS_ENV="staging" /opt/ruby/bin/bundle exec rails runner "puts Jade::Engine.configure_database.to_json"'`
 end
